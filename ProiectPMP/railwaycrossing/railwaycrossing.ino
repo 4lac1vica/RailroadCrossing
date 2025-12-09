7bc531fb93256cf085b4d5d68d59bf2a23ca9dd4
4lac1vica <hp922050@gmail.com>
2025-12-09 23:13:53 +0200

    functii de leduri + buzzer + testare senzori hall

--- ProiectPMP/railwaycrossing/railwaycrossing.ino
+++ ProiectPMP/railwaycrossing/railwaycrossing.ino
@@ -1,9 +1,68 @@
-void setup() {
-  // put your setup code here, to run once:
 
+
+
+
+
+int ledStanga1 = D0;
+int ledDreapta1 = D1;
+
+
+int ledAlb1 = D2;
+
+
+
+
+const int hallPin1 = D5;
+
+
+int buzzer = D8;
+
+void setup() {
+  pinMode(ledStanga1, OUTPUT);
+  pinMode(ledAlb1, OUTPUT);
+  pinMode(ledDreapta1, OUTPUT);  
+  pinMode(hallPin1, INPUT_PULLUP);
+  Serial.begin(74880);
 }
+
+void albIntermitent(){
+  delay(1000);
+  digitalWrite(ledAlb1, HIGH);
+  delay(1000);
+  digitalWrite(ledAlb1, LOW);
+  
+}
+
+
+void rosuIntermitent(){
+  
+  digitalWrite(ledStanga1, HIGH);
+  tone(buzzer, 100);
+  delay(1000);
+  noTone(buzzer);
+  digitalWrite(ledStanga1, LOW);
+  digitalWrite(ledDreapta1, HIGH);
+  tone(buzzer, 100);
+  delay(1000);
+  digitalWrite(ledDreapta1, LOW);
+  noTone(buzzer);
+  
+  
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+   int valoare = digitalRead(hallPin1);
+   Serial.println(valoare);
+   if (valoare == HIGH){
+    albIntermitent();
+   }
+   else if(valoare == LOW){
+    rosuIntermitent();
+   }
+    
 }
+
+  
+
