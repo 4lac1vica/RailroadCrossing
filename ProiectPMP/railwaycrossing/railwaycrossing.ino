b9aa40c9618e1df88f0f69e41c29a6a7f10267fe
4lac1vica <hp922050@gmail.com>
2025-12-12 10:22:02 +0200

    testare senzori effect hall

--- ProiectPMP/railwaycrossing/railwaycrossing.ino
+++ ProiectPMP/railwaycrossing/railwaycrossing.ino
@@ -16,8 +16,10 @@ Servo bariera1, bariera2, bariera3, bariera4;
 
 const int hallPin1 = D5;
 bool barrierStateSens = false;
-bool barrierStateContrasens = false;
+//bool barrierStateContrasens = false;
 
+
+//modificam codul pentru a face ca procesele sa poata sa foloseasca procese in paralel
 int buzzer = D8;
 
 void setup() {
@@ -29,15 +31,15 @@ void setup() {
 
 
   bariera1.attach(D6);
-  bariera3.attach(D7);
-  bariera2.attach(D3);
-  bariera4.attach(D4);
+  //bariera3.attach(D7);
+  //bariera2.attach(D3);
+  //bariera4.attach(D4);
 
   
   bariera1.write(90);
-  bariera2.write(90);
-  bariera3.write(90);
-  bariera4.write(90);
+  //bariera2.write(90);
+  //bariera3.write(90);
+  //bariera4.write(90);
 }
 
 
@@ -45,7 +47,7 @@ void coboaraBarieraSens(){
 
   for (int unghi = 90; unghi >= 0; unghi--){
     bariera1.write(unghi);
-    bariera3.write(unghi);
+   // bariera3.write(unghi);
     delay(10);
   }
 
@@ -55,13 +57,13 @@ void coboaraBarieraSens(){
 void ridicaBarieraSens(){
   for (int unghi = 0; unghi <= 90; unghi++){
     bariera1.write(unghi);
-    bariera3.write(unghi);
+  //  bariera3.write(unghi);
     delay(10);
   } 
 
   barrierStateSens = false;
 }
-
+/*
 void coboaraBarieraContrasens(){
   for (int unghi = 90; unghi >= 0; unghi--){
     bariera2.write(unghi);
@@ -71,7 +73,8 @@ void coboaraBarieraContrasens(){
 
   barrierStateContrasens = true;
 }
-
+*/
+/*
 void ridicaBarieraContrasens(){
   for (int unghi = 0; unghi <= 90; unghi++){
     bariera2.write(unghi);
@@ -81,7 +84,7 @@ void ridicaBarieraContrasens(){
 
   barrierStateContrasens = false;
 }
-
+*/
 void albIntermitent(){
   
   digitalWrite(ledAlb, HIGH); 
@@ -115,19 +118,19 @@ void loop() {
    Serial.println(valoare);
    if (valoare == HIGH){
     albIntermitent();
-    if(barrierStateSens && barrierStateContrasens){
+    if(barrierStateSens){
       ridicaBarieraSens();
       
-      ridicaBarieraContrasens();
+    //  ridicaBarieraContrasens();
     }
    }
    else if(valoare == LOW){
     rosuIntermitent();
     
-    if(!barrierStateSens && !barrierStateContrasens){
+    if(!barrierStateSens){
       coboaraBarieraSens();
       
-      coboaraBarieraContrasens();
+    //  coboaraBarieraContrasens();
     }
     
    }
